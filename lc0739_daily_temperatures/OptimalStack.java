d64a76766ed6b988f918b62e9416878a5c0cd61b
Durai85 <mduraimurugan@karunya.edu.in>
2026-02-12 10:21:01 +0530

    Leetcode : 0739

---
+++ lc0739_daily_temperatures/OptimalStack.java
@@ -0,0 +1,19 @@
+package lc0739_daily_temperatures;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class OptimalStack {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] result = new int[temperatures.length];
+        for(int i=0; i<temperatures.length; i++){
+            while(!stack.isEmpty() && temperatures[stack.peek()]<temperatures[i]){
+                int prev = stack.pop();
+                result[prev] = i - prev;
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
