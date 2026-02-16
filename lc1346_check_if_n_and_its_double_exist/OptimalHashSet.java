d6d9540341523a5e677b0f2a728aed41ce27ad8d
Durai85 <mduraimurugan@karunya.edu.in>
2026-02-16 07:15:28 +0530

    Leetcode : 1346

---
+++ lc1346_check_if_n_and_its_double_exist/OptimalHashSet.java
@@ -0,0 +1,17 @@
+package lc1346_check_if_n_and_its_double_exist;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class OptimalHashSet {
+    public boolean checkIfExist(int[] arr) {
+        Set<Integer> seen = new HashSet<>();
+        for(int x : arr){
+            if(seen.contains(x*2) || (x%2==0) && seen.contains(x/2)){
+                return true;
+            }
+            seen.add(x);
+        }
+        return false;
+    }
+}
