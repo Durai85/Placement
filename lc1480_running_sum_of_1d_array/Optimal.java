0af07fde38800d926a4fa925e2edb806031fc9fa
Durai85 <mduraimurugan@karunya.edu.in>
2026-02-17 06:56:07 +0530

    Leetcode : 1480

---
+++ lc1480_running_sum_of_1d_array/Optimal.java
@@ -0,0 +1,10 @@
+package lc1480_running_sum_of_1d_array;
+
+public class Optimal {
+    public int[] runningSum(int[] nums) {
+        for(int i=1; i<nums.length; i++){
+            nums[i] += nums[i-1];
+        }
+        return nums;
+    }
+}
