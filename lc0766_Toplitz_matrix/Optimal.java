d16fc359625bb55bb9208dc12e5bf466705bf2e5
Durai85 <mduraimurugan@karunya.edu.in>
2026-02-19 22:08:40 +0530

    Leetcode : 0766

---
+++ lc0766_Toplitz_matrix/Optimal.java
@@ -0,0 +1,14 @@
+package lc0766_Toplitz_matrix;
+
+public class Optimal {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(matrix[i][j]!=matrix[i-1][j-1]) return false;
+            }
+        }
+        return true;
+    }
+}
