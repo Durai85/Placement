7e5c2a61f324bc31650957abc9407c97cf4a96d5
Durai85 <mduraimurugan@karunya.edu.in>
2026-02-20 23:24:21 +0530

    Leetcode : 0028

---
+++ lc0028_find_the_index_of_the_first_occurance_in_a_string/Optimal.java
@@ -0,0 +1,7 @@
+package lc0028_find_the_index_of_the_first_occurance_in_a_string;
+
+public class Optimal {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
