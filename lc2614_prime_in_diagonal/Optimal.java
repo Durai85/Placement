de41cb6319e8cb4ee2892e56307c3740780afd72
Durai85 <mduraimurugan@karunya.edu.in>
2026-02-20 21:43:36 +0530

    Leetcode : 2614

---
+++ lc2614_prime_in_diagonal/Optimal.java
@@ -0,0 +1,24 @@
+package lc2614_prime_in_diagonal;
+
+public class Optimal {
+    public int diagonalPrime(int[][] nums) {
+        int max = 0;
+        for(int i=0; i<nums.length; i++){
+            if(isPrime(nums[i][i])){
+                max = Math.max(max, nums[i][i]);
+            }
+            if(isPrime(nums[i][nums.length-i-1])){
+                max = Math.max(max, nums[i][nums.length-i-1]);
+            }
+        }
+        return max;
+    }
+
+    private boolean isPrime(int num){
+        if(num < 2) return false;
+        for(int i=2; i*i <= num; i++){
+            if(num % i==0) return false;
+        }
+        return true;
+    }
+}
