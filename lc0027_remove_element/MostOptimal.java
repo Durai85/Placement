a8d16de1aa114990b67ab0e4b19d8eb4e9b78856
Durai85 <mduraimurugan@karunya.edu.in>
2026-02-22 09:05:40 +0530

    Leetcode : 0027

---
+++ lc0027_remove_element/MostOptimal.java
@@ -0,0 +1,13 @@
+package lc0027_remove_element;
+
+public class MostOptimal {
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=val){
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+}
