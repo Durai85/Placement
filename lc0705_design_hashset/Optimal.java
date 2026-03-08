24aa146b03ab41b71b87864f347330db3e06ff3f
Durai85 <mduraimurugan@karunya.edu.in>
2026-03-08 09:56:13 +0530

    Leetcode : 0705

---
+++ lc0705_design_hashset/Optimal.java
@@ -0,0 +1,24 @@
+package lc0705_design_hashset;
+
+public class Optimal {
+    class MyHashSet {
+
+    boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        set[key] = true;
+    }
+    
+    public void remove(int key) {
+        set[key] = false;
+    }
+    
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+}
