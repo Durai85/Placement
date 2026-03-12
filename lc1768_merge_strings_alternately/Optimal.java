97830cefcb3084117d0a8eb5ab303f354b1f31a0
Durai85 <mduraimurugan@karunya.edu.in>
2026-03-12 10:57:59 +0530

    Leetcode : 1768

---
+++ lc1768_merge_strings_alternately/Optimal.java
@@ -0,0 +1,19 @@
+package lc1768_merge_strings_alternately;
+
+public class Optimal {
+    public String mergeAlternately(String word1, String word2) {
+        int i=0, j=0;
+        StringBuilder str = new StringBuilder();
+        while(i < word1.length() && j < word2.length()){
+            str.append(word1.charAt(i++));
+            str.append(word2.charAt(j++));
+        }
+        while(i < word1.length()){
+            str.append(word1.charAt(i++));
+        }
+        while(j < word2.length()){
+            str.append(word2.charAt(j++));
+        }
+        return new String(str);
+    }
+}
