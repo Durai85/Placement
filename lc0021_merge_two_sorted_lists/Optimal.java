2c28590df752108ac42f0f99c72288b6034afcfa
Durai85 <mduraimurugan@karunya.edu.in>
2026-03-23 21:37:13 +0530

    Leetcode : 0021

---
+++ lc0021_merge_two_sorted_lists/Optimal.java
@@ -0,0 +1,41 @@
+package lc0021_merge_two_sorted_lists;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class Optimal {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode result = new ListNode(66);
+        ListNode ans = result;
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                result.next = new ListNode(list1.val);
+                list1 = list1.next;
+            }
+            else{
+                result.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            result = result.next;
+        }
+
+        while(list1 != null){
+            result.next = new ListNode(list1.val);
+            list1 = list1.next;
+            result = result.next;
+        }
+
+        while(list2 != null){
+            result.next = new ListNode(list2.val);
+            list2 = list2.next;
+            result = result.next;
+        }
+
+        return ans.next;
+    }
+}
