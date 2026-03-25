92a506709dc5f8152013a765eee4d26d27617e11
Durai85 <mduraimurugan@karunya.edu.in>
2026-03-25 11:12:40 +0530

    Leetcode : 0094

---
+++ lc0094_binary_tree_inorder_traversal/Optimal.java
@@ -0,0 +1,30 @@
+package lc0094_binary_tree_inorder_traversal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Optimal {
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null) return result;
+
+        inorderTraversal(root.left);
+        result.add(root.val);
+        inorderTraversal(root.right);
+        
+        return result;
+    }
+}
